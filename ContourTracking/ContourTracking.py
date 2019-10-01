db2848b3fb59c2742482ed6feea166ea464f1049
OrestChura <orest-chura@yandex.ru>
2019-10-01 20:13:15 +0300

    - Null image subtraction implemented - 2 implementations of good features search for every new point while manually drawing contour added - some bugs

--- ContourTracking/ContourTracking.py
+++ ContourTracking/ContourTracking.py
@@ -61,9 +61,16 @@ def thresholdOtsu(pict):
 def getPictTrue(name, i, wl):
     pictFluo = cv.imread(name + str(i) + '_' + str(wl) + '.tiff', cv.IMREAD_UNCHANGED)
     pictNull = cv.imread(name + str(i) + '_0.tiff', cv.IMREAD_UNCHANGED)
-    #return pictFluo - pictNull
+    pictFluo = (pictFluo - pictNull) * (pictFluo > pictNull)
+    #return np.array([i if j else 0 for (k, l) in zip((pictFluo - pictNull), (pictFluo > pictNull)) for (i, j) in zip(k, l)]).reshape(shape)
+    #return np.array([i if j else 0 for (i, j) in zip((pictFluo - pictNull).reshape(-1), (pictFluo > pictNull).reshape(-1))]).reshape(shape)
+    # for i in range(pictFluo.shape[0]):
+    #     for j in range(pictFluo.shape[1]):
+    #         if pictFluo[i, j] < pictNull[i, j]:
+    #             pictFluo[i, j] = 0
+    #         else:
+    #             pictFluo[i, j] -= pictNull[i, j]
     return pictFluo
-
 
 # WRAPPER
 def trackContourPyrLK(pictPrev, pictNext, ptsPrev, winSize, maxLevel):
@@ -81,13 +88,7 @@ def getNumberHighIntensityPixels(pict):
 def get740picts(name, begin, end):
     vctPicts = []
     for i in range(begin, end + 1):
-        pictTrue = getPictTrue(name, i, 740)
-
-        maxPictTrue = np.max(pictTrue)
-        #if maxPictTrue > maxAllowedIntensity:
-        #    pictTrue = cutHighIntensityPoints(pictTrue, maxAllowedIntensity)
-
-        vctPicts.append(pictTrue)
+        vctPicts.append(getPictTrue(name, i, 740))
         print(str(i) + '/' + str(end))
     return vctPicts
 
@@ -125,18 +126,17 @@ def trackOneStepMeanShift(pictPrev, ptsPrev, pictNext, winSize, maxLevel, delta)
 def trackSeriesCompare740(winSize, maxLevel, delta, compare=False):
     name, begin, end = inputImages()
     vctPicts = get740picts(name, begin, end)
-    print np.max(vctPicts[0]).astype(np.float32)
-    #vctPictsToShow = [(p * (maxIntensity / max(np.max(p).astype(np.float32), 1).astype(np.float32))).astype(np.uint8) for p in vctPicts]
     vctPictsToShow = [(p * (maxIntensity / max(np.max(p).astype(np.float32), 1).astype(np.float32))).astype(np.uint8) for p in vctPicts]
 
 
     cv.namedWindow('contoured', cv.WINDOW_NORMAL)
 
     contPrev = contFirst = np.array(contourManual((vctPictsToShow[0]))).reshape((-1, 1, 2))
+
     maxDotsOut = contPrev.shape[0] / 2
 
     pictToShow = cv.cvtColor(vctPictsToShow[0], cv.COLOR_GRAY2RGB)
-    cv.drawContours(pictToShow, [contPrev], -1, CLR_RED, 3)
+    cv.drawContours(pictToShow, [contPrev.astype(np.int32)], -1, CLR_RED, 3)
     cv.imshow('contoured', pictToShow)
 
     if cv.waitKey(0) == BTN_CTRL_C:
@@ -144,16 +144,16 @@ def trackSeriesCompare740(winSize, maxLevel, delta, compare=False):
         cv.waitKey(0)
     numBadPictures = numLazeredPictures = 0
     for i in range(1, len(vctPicts)):
-        print('frame: ' + str(i))
+        print('frame: ' + str(i + 1))
 
-        shift = trackOneStepMeanShift(vctPictsToShow[i - numBadPictures - numLazeredPictures - 1],
-                                contPrev, vctPictsToShow[i], winSize, maxLevel, delta)
-        if not shift == []:  # everything OK, go to usual loop, null counters
-            contNext = (contPrev + shift)
-        # contNext = trackOneStep(vctPictsToShow[i - numBadPictures - numLazeredPictures - 1],
-        #                         contPrev, vctPictsToShow[i],
-        #                         winSize, maxLevel, delta)
-        # if contPrev.shape[0] - contNext.shape[0] <= maxDotsOut:  # everything OK, go to usual loop, null counters
+        # shift = trackOneStepMeanShift(vctPictsToShow[i - numBadPictures - numLazeredPictures - 1],
+        #                         contPrev, vctPictsToShow[i], winSize, maxLevel, delta)
+        # if not shift == []:  # everything OK, go to usual loop, null counters
+        #     contNext = (contPrev + shift)
+        contNext = trackOneStep(vctPictsToShow[i - numBadPictures - numLazeredPictures - 1],
+                                contPrev, vctPictsToShow[i],
+                                winSize, maxLevel, delta)
+        if contPrev.shape[0] - contNext.shape[0] <= maxDotsOut:  # everything OK, go to usual loop, null counters
             pictToShow = cv.cvtColor(vctPictsToShow[i], cv.COLOR_GRAY2RGB)
             cv.drawContours(pictToShow, [contNext.astype(np.int32)], -1, CLR_RED, 3)
 
@@ -175,10 +175,25 @@ def trackSeriesCompare740(winSize, maxLevel, delta, compare=False):
     if compare:
         cv.namedWindow('firstpicture', cv.WINDOW_NORMAL)
         pictFirst = cv.cvtColor(vctPictsToShow[0], cv.COLOR_GRAY2RGB)
-        cv.drawContours(pictFirst, [contFirst], -1, CLR_RED, 3)
+        cv.drawContours(pictFirst, [contFirst.astype(np.int32)], -1, CLR_RED, 3)
         cv.imshow('firstpicture', pictFirst)
         if cv.waitKey(0) == BTN_CTRL_C:
             cv.imwrite('Output\\' + name + str(1) + '.tiff', pictToShow)
+
+
+def getABetterPointToTrackGoodFeatures(x, y):
+    mask = np.zeros(dictParams["shape"], np.uint8)
+    mask[x - dictParams["kernelSize"] / 2: x + dictParams["kernelSize"] / 2 + 1,
+    y - dictParams["kernelSize"] / 2: y + dictParams["kernelSize"] / 2 + 1] = \
+        dictParams["kernel"]
+    return cv.goodFeaturesToTrack(dictParams["pictNormalized"], 1, 0.1, 0,
+                                  mask=mask).reshape(2)[::-1]
+
+def getABetterPointToTrackSubPix(x, y):
+    point = np.array([x, y], np.float32).reshape(1, 2)
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_COUNT, 30, 0.001)
+    cv.cornerSubPix(dictParams["pictNormalized"], point, tuple((5,5)), zeroZone=tuple((-1, -1)), criteria=criteria)
+    return point.reshape(2)
 
 
 # poly = [[x1,y1],...] = man_con()
@@ -188,25 +203,33 @@ def contourManual(pict):
 
     global dictParams
     dictParams = {"color":          CLR_GREEN,
-                  "pictNormalized": (cv.cvtColor(pict, cv.COLOR_GRAY2RGB).astype(np.float)
-                                     / max(np.max(cv.cvtColor(pict, cv.COLOR_GRAY2RGB)), 1)
+                  "pictNormalized": (pict.astype(np.float) / max(np.max(pict), 1)
                                      * maxIntensity).astype(np.uint8),
                   "poly":           [],
+                  "shape":          pict.shape,
                   "flgDrawing":     False,
-                  "thickness":      3}
-    dictParams["pictShow"] = dictParams["pictNormalized"].copy()
+                  "thickness":      3,
+                  "kernelSize":     11}
+
+    dictParams["kernel"] = np.ones((dictParams["kernelSize"], dictParams["kernelSize"]), np.uint8)
+    dictParams["pictShow"] = cv.cvtColor(dictParams["pictNormalized"], cv.COLOR_GRAY2RGB)
 
     def onMouse(event, x, y, flags, param):
         global dictParams
         if event == cv.EVENT_LBUTTONDOWN:
             dictParams["flgDrawing"] = True
-            dictParams["poly"] = dictParams["poly"] + [[x, y]]
+            # dictParams["poly"] = dictParams["poly"] + [getABetterPointToTrackGoodFeatures(x, y)]
+            dictParams["poly"] = dictParams["poly"] + [getABetterPointToTrackSubPix(x, y)]
+            # dictParams["poly"] = dictParams["poly"] + [[x, y]]
+            dictParams["pictShow"] = cv.cvtColor(dictParams["pictNormalized"], cv.COLOR_GRAY2RGB)
             cv.polylines(dictParams["pictShow"], [np.array(dictParams["poly"], np.int32)],
                          False, dictParams["color"], dictParams["thickness"])
             cv.imshow('input', dictParams["pictShow"])
         elif event == cv.EVENT_MOUSEMOVE:
             if dictParams['flgDrawing'] is True:
-                dictParams["poly"] = dictParams["poly"] + [[x, y]]
+                # dictParams["poly"] = dictParams["poly"] + [getABetterPointToTrackGoodFeatures(x, y)]
+                # dictParams["poly"] = dictParams["poly"] + [[x, y]]
+                dictParams["poly"] = dictParams["poly"] + [getABetterPointToTrackSubPix(x, y)]
                 cv.line(dictParams["pictShow"], tuple(dictParams["poly"][-2]), tuple(dictParams["poly"][-1]),
                         dictParams["color"], dictParams["thickness"])
                 cv.imshow('input', dictParams["pictShow"])
@@ -217,7 +240,7 @@ def contourManual(pict):
             cv.imshow('input', dictParams["pictShow"])
         elif event == cv.EVENT_RBUTTONUP:
             dictParams["poly"] = []
-            dictParams["pictShow"] = dictParams["pictNormalized"].copy()
+            dictParams["pictShow"] = cv.cvtColor(dictParams["pictNormalized"], cv.COLOR_GRAY2RGB)
             cv.imshow('input', dictParams["pictShow"])
 
     while not dictParams["poly"]:
